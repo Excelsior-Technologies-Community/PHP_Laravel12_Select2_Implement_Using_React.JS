55f451748c07983525873842fc5c421d17633c2f
YashPatel4268 <142138437+YashPatel4268@users.noreply.github.com>
2026-04-17 11:31:38 +0530

    feat(employee-crud): implement search, restore soft delete, pagination, and enhance UI with skill badges

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,10 @@
 use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 
 Route::get('/', [EmployeeController::class, 'index']);
 Route::post('/store', [EmployeeController::class, 'store']);
 Route::post('/update/{id}', [EmployeeController::class, 'update']);
 
 Route::post('/delete/{id}', [EmployeeController::class, 'destroy']);
+Route::post('/restore/{id}', [EmployeeController::class, 'restore']);
